f4e676d199abbc39eef67c231e143dd9bb403667
NOGUCHI HIROKI <t-cool@t-cool.lan>
2024-12-07 13:09:38 +0900

    init

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("こんにちは、世界！");
+}
+
+#[wasm_bindgen]
+pub fn fibonacci(n: u32) -> Vec<u32> {
+    let mut sequence = vec![0, 1];
+    for i in 2..=n as usize {
+        let next = sequence[i - 1] + sequence[i - 2];
+        sequence.push(next);
+    }
+    sequence.truncate((n + 1) as usize);
+    sequence
+}
