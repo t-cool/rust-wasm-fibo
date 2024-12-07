9a63c11e344c2aee0a59d3990358b73d348d30d8
t-cool <hiroki802@hotmail.com>
2024-12-07 13:10:18 +0900

    Add files via upload

---
+++ pkg/hello_wasm_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const greet: () => void;
+export const fibonacci: (a: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
